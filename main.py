7dcb7829a43f0bc3a2becaf016aa0343d57d1166
Rickard Eriksson <rickard.eriksson@gigabit.nu>
2018-11-13 22:02:59 +0100

    added more smilies

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from ev3dev.ev3 import *
 
 def main():
     while True:
-    	print("Five in the main and four in the __name__! *-<:)")
+    	print("Five in the main and four in the __name__! *-<:) *-<:)")
 
     return True
 
