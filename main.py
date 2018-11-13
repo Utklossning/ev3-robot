81eb61f5fa5e48fc985876df2a3c6544cb776bc2
Rickard Eriksson <rickard.eriksson@gigabit.nu>
2018-11-13 21:41:48 +0100

    added while loop to keep program from exiting on run

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 from ev3dev.ev3 import *
 
 def main():
-	print("Five in the main and four in the __name__!")
+    while True:
+    	print("Five in the main and four in the __name__!")
+
     return True
 
 if __name__ == "__main__":
