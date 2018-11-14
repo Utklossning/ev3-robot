9772b3621253afd062149e1f6e629b251c329f0a
Martin Hoshi Larsson <martin850102@gmail.com>
2018-11-14 09:37:25 +0100

    Started creating real files and sound

---
+++ bot.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+# NATIVE IMPORTS
+from math import pi
+
+# THIRT PARTY IMPORTS
+from ev3dev2.motor import \
+    LargeMotor, SpeedRPS, MoveSteering, \
+    OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.sensor.lego import TouchSensor, UltrasonicSensor, ColorSensor
+from ev3dev2.sound import Sound
+
+# CONSTANTS
+FORWARD = 0
+LEFT_ROTATION = -100
+RIGHT_ROTATION = 100
+
+# DEFINITIONS
+class Bot:
+    def __init__(self, wheel_radius, wheel_spacing):
+        self._left_motor = self._init_motor(OUTPUT_A)
+        self._right_motor = self._init_motor(OUTPUT_B)
+        self._steering_drive = MoveSteering(OUTPUT_A, OUTPUT_B)
+        self._touch_sensor = TouchSensor(INPUT_1)
+        self._ultrasonic_sensor = UltrasonicSensor(INPUT_2)
+        self._color_sensor = ColorSensor(INPUT_3)
+        self._color_sensor.mode = "RGB-RAW"
+        #self._color_sensor.mode = "RGB-AMBIENT"
+        self.WHEEL_RADIUS = wheel_radius
+        self.WHEEL_SPACING = wheel_spacing
+
+    def _init_motor(self, output):
+        m = LargeMotor(output)
+        m.ramp_up_sp = 1000
+        m.ramp_down_sp = 1000
+        return m
+
+    def read_touch(self):
+        return self._touch_sensor.is_pressed
+
+    def read_ultrasonic(self):
+        return self._ultrasonic_sensor.distance_centimeters
+
+    def read_color(self):
+        if self._color_sensor.mode == "RGB-RAW":
+            return tuple(map(self._color_sensor.value, [0,1,2]))
+        elif self._color_sensor.mode == "RGB-AMBIENT":
+            return self._color_sensor.value()
+
+    def _cm_movement_to_rotations(self, distance):
+        return distance / (pi*2*self.WHEEL_RADIUS)
+
+    def move_forward(self, distance, speed_percent, blocking=True):
+        rots = _cm_movement_to_rotations(distance)
+        self._steering_drive.on_for_rotations(
+            FORWARD, speed_percent, rots, block=blocking)
+
+    def rotate_left(self, degrees, speed_percent, blocking=True):
+        distance = self.WHEEL_SPACING * pi * degrees / 360
+        rots = _cm_movement_to_rotations(distance)
+        self._steering_drive.on_for_rotations(
+            LEFT_ROTATION, speed_percent, rots, block=blocking)
+
+    def rotate_right(self, degrees, speed_percent, blocking=True):
+        distance = self.WHEEL_SPACING * pi * degrees / 360
+        rots = _cm_movement_to_rotations(distance)
+        self._steering_drive.on_for_rotations(
+            RIGHT_ROTATION, speed_percent, rots, block=blocking)
+
+    def wav_processor(self):
+        Sound.play('t2_learning_computer_x.wav')
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-#!/usr/bin/python3
-from ev3dev.ev3 import *
+#!/usr/bin/env python3
+
+# LOCAL IMPORTS
+import bot
 
 def main():
-    while True:
-    	print("Five in the main and four in the __name__! *-<:) *-<:)")
-
-    return True
+    myBot = bot.Bot(wheel_radius=2.3, wheel_spacing=10.5)
+    myBot.move_forward(distance=20, speed_percent=100)
+    myBot.rotate_left(degrees=360, speed_percent=20)
+    myBot.rotate_right(degrees=720, speed_percent=50)
+    myBot.rotate_left(degrees=360, speed_percent=100)
+    myBot.wav_processor()
 
 if __name__ == "__main__":
     main()
