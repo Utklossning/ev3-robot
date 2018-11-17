946cee7ce5134a538e5fbfa1583dcb959b5d4ca8
Rickard Eriksson <rickard@dynamist.se>
2018-11-17 14:43:03 +0100

    route updates

--- routes/route2.py
+++ routes/route2.py
@@ -7,7 +7,7 @@ class Route():
     
     def start(self):
         self.bot.move_forward(34, 50)
-        self.bot.rotate_right(90, 30)
+        self.bot.rotate_right(89, 30)
         self.bot.move_forward(57, 50)
         self.bot.detect_red_tape()
         self.bot.move_forward(3, 20)
--- routes/route3.py
+++ routes/route3.py
@@ -6,14 +6,14 @@ class Route():
         self.route_number = "three"
     
     def start(self):
-        self.bot.move_forward(27, 50)
+        self.bot.move_forward(25, 50)
         self.bot.rotate_right(45, 30)
-        self.bot.move_forward(18, 50)
+        self.bot.move_forward(21, 50)
         time.sleep(0.5)
         self.bot.empty_container()
-        self.bot.move_backward(18, 50)
+        self.bot.move_backward(21, 50)
         self.bot.rotate_left(35, 30)
-        self.bot.move_backward(52, 50)
+        self.bot.move_backward(45, 50)
 
         return True 
 
