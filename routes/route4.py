2e8438c8344c17c06ef584b298873fbeef8ad262
Rickard Eriksson <rickard@dynamist.se>
2018-11-17 12:56:32 +0100

    ain't nobody got time fo that

--- routes/route4.py
+++ routes/route4.py
@@ -1,9 +1,12 @@
+import time 
+
 class Route():
     def __init__(self, bot):
         self.bot = bot
         self.route_number = "four"
     
     def start(self):
-        run = True
+        self.bot.move_forward(150, 50)
+        return True
 
     
--- routes/route5.py
+++ routes/route5.py
@@ -1,3 +1,5 @@
+import time 
+
 class Route():
     def __init__(self, bot):
         self.bot = bot
