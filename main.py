4ec4beb084d9bb94cf7640e7ef43dd4c4fccc406
Rickard Eriksson <rickard@dynamist.se>
2018-11-17 09:42:30 +0100

    cleanup

--- main.py
+++ main.py
@@ -2,24 +2,28 @@
 
 # NATIVE IMPORTS
 from time import sleep
+from routes.route1 import Route as route1
 
-# LOCAL IMPORTS
-import bot
+## LOCAL IMPORTS
+#import bot
+#
+#def main():
+#    myBot = bot.Bot(wheel_radius=2.3, wheel_spacing=10.5)
+#    myBot.move_forward(distance=20, speed_percent=20)
+#    myBot.rotate_left(degrees=360, speed_percent=50)
+#    myBot.rotate_right(degrees=720, speed_percent=50)
+#    myBot.rotate_left(degrees=360, speed_percent=50)
+#    for i in range(100):
+#        myBot.rotate_left(degrees=3.6, speed_percent=10)
+#    myBot.wav_processor()
+#    sleep(5)
+#    print(myBot.read_touch())
+#    print(myBot.read_ultrasonic())
+#    print(myBot.read_color())
+#
 
 def main():
-    myBot = bot.Bot(wheel_radius=2.3, wheel_spacing=10.5)
-    myBot.move_forward(distance=20, speed_percent=20)
-    myBot.rotate_left(degrees=360, speed_percent=50)
-    myBot.rotate_right(degrees=720, speed_percent=50)
-    myBot.rotate_left(degrees=360, speed_percent=50)
-    for i in range(100):
-        myBot.rotate_left(degrees=3.6, speed_percent=10)
-    myBot.wav_processor()
-    sleep(5)
-    print(myBot.read_touch())
-    print(myBot.read_ultrasonic())
-    print(myBot.read_color())
-
+    print(route1)
 
 if __name__ == "__main__":
     main()
