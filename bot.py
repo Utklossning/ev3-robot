a46bb9c4d363c000aa61d8f30c15bcd592348fd2
Rickard Eriksson <rickard@dynamist.se>
2018-11-17 12:27:13 +0100

    slow down there buster, going over the speedlimit!

--- bot.py
+++ bot.py
@@ -29,7 +29,6 @@ class Bot:
     def __init__(self, wheel_radius, wheel_spacing):
         self._container_motor = MediumMotor(OUTPUT_C)
         self._steering_drive = MoveSteering(OUTPUT_D, OUTPUT_A)
-        self._steering_drive.ramp_up_sp = 10000
         self._touch_sensor_front = TouchSensor(INPUT_1)
         self._touch_sensor_top = TouchSensor(INPUT_2)
         #self._ultrasonic_sensor = UltrasonicSensor(INPUT_2)
--- routes/route1.py
+++ routes/route1.py
@@ -4,9 +4,9 @@ class Route():
         self.route_number = "one"
     
     def start(self):
-        self.bot.move_forward(56.5, 100) 
+        self.bot.move_forward(56.5, 50) 
         self.bot.empty_container()
-        self.bot.move_backward(56.5, 100)
+        self.bot.move_backward(56.5, 50)
         return True
 
     
