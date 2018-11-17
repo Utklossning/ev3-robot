023307291a22e2fd76d7c861385e7f541dc34e1f
Rickard Eriksson <rickard@dynamist.se>
2018-11-17 14:57:06 +0100

    asdfasdf

--- routes/route4.py
+++ routes/route4.py
@@ -3,22 +3,27 @@ import time
 class Route():
     def __init__(self, bot):
         self.bot = bot
-        self.route_number = "four"
+        self.route_number = "five"
     
     def start(self):
-        self.bot.move_forward(36, 50)
-        self.bot.rotate_right(90, 50)
-        self.bot.move_forward(55, 50)
-        self.bot.rotate_right(92, 50)
-        self.bot.move_forward(29, 50)
-        time.sleep(0.5)
+        self.bot.move_forward(45, 50)
+        self.bot.rotate_right(45, 50)
+        self.bot.move_forward(44, 50)
+        self.bot.rotate_right(46, 50)
+        self.bot.move_forward(32, 50)
+        self.bot.detect_red_tape()
         self.bot.empty_container()
-        self.bot.move_backward(35, 50)
-        self.bot.rotate_left(90, 50)
-        self.bot.move_backward(55, 50)
-        self.bot.rotate_left(82, 50)
-        self.bot.move_backward(28, 50)
-
+        self.bot.move_backward(40, 50)
+        self.bot.rotate_left(46, 50)
+        self.bot.move_backward(44, 50)
+        self.bot.rotate_left(37, 50)
+        self.bot.move_backward(50, 50)
+    
         return True
+        
+        
+
+           
+        
 
     
--- routes/route5.py
+++ routes/route5.py
@@ -3,27 +3,22 @@ import time
 class Route():
     def __init__(self, bot):
         self.bot = bot
-        self.route_number = "five"
+        self.route_number = "four"
     
     def start(self):
-        self.bot.move_forward(45, 50)
-        self.bot.rotate_right(45, 50)
-        self.bot.move_forward(44, 50)
-        self.bot.rotate_right(46, 50)
-        self.bot.move_forward(40, 50)
-        self.bot.detect_red_tape()
+        self.bot.move_forward(36, 50)
+        self.bot.rotate_right(90, 50)
+        self.bot.move_forward(55, 50)
+        self.bot.rotate_right(92, 50)
+        self.bot.move_forward(29, 50)
+        time.sleep(0.5)
         self.bot.empty_container()
-        self.bot.move_backward(40, 50)
-        self.bot.rotate_left(46, 50)
-        self.bot.move_backward(44, 50)
-        self.bot.rotate_left(37, 50)
-        self.bot.move_backward(50, 50)
-    
-        return True
-        
-        
+        self.bot.move_backward(35, 50)
+        self.bot.rotate_left(90, 50)
+        self.bot.move_backward(55, 50)
+        self.bot.rotate_left(82, 50)
+        self.bot.move_backward(28, 50)
 
-           
-        
+        return True
 
     
